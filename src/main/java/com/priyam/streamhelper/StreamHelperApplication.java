e5ceaf013dd055912c5517a870facf1d126c329d
priyam kumar das <37584901+priyam9669@users.noreply.github.com>
2024-03-23 20:12:09 +0530

    initaialisse project

---
+++ src/main/java/com/priyam/streamhelper/StreamHelperApplication.java
@@ -0,0 +1,13 @@
+package com.priyam.streamhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyam/streamhelper/StreamHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyam.streamhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
